00adecbc89521417f35c8e132edaaa7ee77d425f
JackBosca <boscarioljacopo@gmail.com>
2025-04-17 23:30:04 +0200

    added CLIP mean and std parameters for images

--- DL_vs_HateSpeech/loading_data/preprocessing.py
+++ DL_vs_HateSpeech/loading_data/preprocessing.py
@@ -1,19 +1,24 @@
 # Here we can add any trasnformations we want to apply to the data
 import torch
-
+    
 class NormalizePixels():
-    """
-    Normalize the image values between 0 and 1.
-        
-    Parameters
-    image(torch.Tensor): The image to normalize.
-    
-    Returns
-    torhc.Tensor: The normalized image.
-    """
-    def __init__(self):
-        pass
-    
+    def __init__(self, clip_normalize=False):
+        self.clip_normalize = clip_normalize
+
+        # Define CLIP normalization parameters
+        if clip_normalize:
+            self.mean = torch.tensor([0.48145466, 0.4578275, 0.40821073])
+            self.std = torch.tensor([0.26862954, 0.26130258, 0.27577711])
+
     def __call__(self, image):
-        # Normalize the image values between 0 and 1
-        return image / 255.0
+        # # RGBA (4-channel) to RGB (3-channel)
+        # if image.shape[0] == 4:
+        #     image = image[:3]  # Discard alpha channel if it exists
+
+        # # Scale to [0, 1]
+        # image = image / 255.0
+
+        # Apply CLIP normalization if specified
+        if self.clip_normalize:
+            image = (image - self.mean[:, None, None]) / self.std[:, None, None]
+        return image
