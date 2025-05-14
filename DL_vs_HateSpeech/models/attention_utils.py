bdefd15a9418bc00b20ae61311a584001eeb23b5
JackBosca <boscarioljacopo@gmail.com>
2025-05-14 16:36:34 +0200

    cleaned attention_utils.py and merged self and cross attention rollout functions

--- DL_vs_HateSpeech/models/attention_utils.py
+++ DL_vs_HateSpeech/models/attention_utils.py
@@ -8,12 +8,13 @@ from DL_vs_HateSpeech.loading_data.dataloader import DataLoader
 from torch.utils.data import DataLoader as TorchDataLoader
 from DL_vs_HateSpeech.training.training import collate_fn
 
-def plot_attention_rollout(path, device="cpu"):
+def plot_attention_rollout(path, self_attn=True, device="cpu"):
     """
     Plot the attention rollout for a given model and inputs.
 
     Args:
         path (str): Path to the model checkpoint.
+        self_attn (bool): Whether to consider self-attention. Default is True.
         device (str): Device to load the model on. Default is "cpu".
     """
     # Load model from path
@@ -23,27 +24,21 @@ def plot_attention_rollout(path, device="cpu"):
     config_dict = read_yaml_file(path)
     print(config_dict)
 
-    # Create the dataloaders
+    # Create the train dataloader
     train_dataset = DataLoader(type="train", subset=config_dict["train"]["data_subset"])
-    test_dataset = DataLoader(type="test", subset=config_dict["train"]["data_subset"])
-
     train_loader = TorchDataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
-    test_loader = TorchDataLoader(test_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
 
     # Get the first batch of training data
     image, text, labels = next(iter(train_loader))
 
     # Print shapes and labels
-    print("Image shape:", image[0].size)
+    print("\n Image shape:", image[0].size, "\n")
     print("Label:", get_label_str_list(labels))
 
     # Perform attention rollout
-    attention_rollout(model, text, image)
+    attention_rollout(model, text, image, self_attn=self_attn)
 
-    # Perform self-attention rollout
-    self_attention_rollout(model, text, image)
-
-def attention_rollout(model, text, image):
+def attention_rollout(model, text, image, self_attn=True):
     """
     Perform attention rollout for a given model and inputs. Considers cross-attention
     between text and image.
@@ -52,6 +47,7 @@ def attention_rollout(model, text, image):
         model: The model to perform attention rollout on. Must have a method called get_model_attention.
         image: The image input for the model.
         text: The text input for the model.
+        self_attn (bool): Whether to consider self-attention. Default is True.
 
     Returns:
         Tensor: The attention weights from the model of dims (text_shape, image_shape).
@@ -67,7 +63,12 @@ def attention_rollout(model, text, image):
 
     # Get the prediction
     pred = torch.round(probs).squeeze(0)
-    print("Prediction:", get_label_str_list(pred))
+    print("Prediction:", get_label_str_list(pred), "\n")
+
+    if self_attn:
+        print("Performing self-attention rollout... \n")
+    else:
+        print("Performing cross-attention rollout... \n")
     
     # Get the dict of attention weights from the model
     attn = model.get_model_attention()
@@ -77,159 +78,62 @@ def attention_rollout(model, text, image):
     clip_image_attn = attn['clip_image']
     classifier_attn = attn['classifier']
 
-    print("Shapes of iterables:", clip_text_attn[0].shape, clip_image_attn[0].shape, classifier_attn[0].shape)
-
     # Get the dimensions of the text and image attention matrices respectively
     dim_text_mat = clip_text_attn[0].shape[2]
     dim_image_mat = clip_image_attn[0].shape[2]
-    print("Dimensions of text and image attention matrices:", dim_text_mat, dim_image_mat, len(clip_text_attn))
-    print("Classifier attention shape:", classifier_attn[0].shape, len(classifier_attn))
 
-    # Extract the attention weights for the image
-    # complete_attn_image = clip_image_attn + tuple([c[:, :dim_image_mat, :].unsqueeze(0) for c in classifier_attn])
-    attn_cumulative_tuple = [classifier_attn[0][:, :dim_image_mat, :]]
-    print("Shape of clip image attention:", attn_cumulative_tuple[0].shape)
-    for i in range(1, len(classifier_attn)):
-        attn_cumulative_tuple.append(classifier_attn[i])
-        print("Shape of classifier attention:", classifier_attn[i].shape)
+    # Extract the attention weights for the image from the classifier
+    if self_attn:
+        attn_cumulative_list = [classifier_attn[0][:, :dim_image_mat, :dim_image_mat]]
+        for i in range(1, len(classifier_attn)):
+            attn_cumulative_list.append(classifier_attn[i][:, :dim_image_mat, :dim_image_mat])
+    else:
+        attn_cumulative_list = [classifier_attn[0][:, :dim_image_mat, :]]
+        for i in range(1, len(classifier_attn)):
+            attn_cumulative_list.append(classifier_attn[i])
     
-    print("Shape of classifier attention:", attn_cumulative_tuple[0].shape, attn_cumulative_tuple[1].shape)
-    complete_attn_image = clip_image_attn + tuple(attn_cumulative_tuple)
-
-    print("Shape of complete attention image:", complete_attn_image[-2].shape, complete_attn_image[-1].shape)
-
-    for ten in complete_attn_image:
-        print("Shape of attention tensor:", ten.shape)
-
-    print("\n")
+    # Add the attention weights to the clip image attention
+    complete_attn_image = clip_image_attn + tuple(attn_cumulative_list)    
     
-    
-    # Perform attention rollout
+    # Initialize the rollout matrix as an identity matrix
     rollout = torch.eye(dim_image_mat).unsqueeze(0)
-    for ten in complete_attn_image:
-        mean_attn = ten
-        if len(mean_attn.shape) == 4: 
-            mean_attn = mean_attn.mean(dim=1)
-        
-        print("Shape of mean attention:", mean_attn.shape)
-        print("Shape of rollout:", rollout.shape)
 
-        id_mat = torch.eye(mean_attn.shape[1]).to(mean_attn.device)
-        if mean_attn.shape[1] == mean_attn.shape[2]:
-            mean_attn = mean_attn + id_mat.unsqueeze(0)
+    # Loop through the attention weights and perform the rollout
+    for attn in complete_attn_image:
+        # Check if the attention weights are 4D (batch_size, num_heads, seq_len, seq_len)
+        # If so, take the mean across the heads
+        if len(attn.shape) == 4: 
+            attn = attn.mean(dim=1)
+
+        # Initialize the identity matrix
+        id_mat = torch.eye(attn.shape[1]).to(attn.device)
+
+        # Check if the attention weights are square
+        # If so, add the identity matrix to the attention weights
+        if attn.shape[1] == attn.shape[2]:
+            attn = attn + id_mat.unsqueeze(0)
         else:
-            extra_zeros = torch.zeros(mean_attn.shape[1], dim_text_mat).to(mean_attn.device)
-
-            print("\n Shape of extra zeros:", extra_zeros.shape)
-            print("Shape of id_mat:", id_mat.shape)
-
+            # If not, add the identity matrix to the attention weights
+            # and add extra zeros to match the dimensions
+            extra_zeros = torch.zeros(attn.shape[1], dim_text_mat).to(attn.device)
             id_rect = torch.cat((id_mat, extra_zeros), dim=-1)
-            
-            print("Shape of id_rect:", id_rect.shape)
-            mean_attn = mean_attn + id_rect.unsqueeze(0)
+            attn = attn + id_rect.unsqueeze(0)
 
-        mean_attn /= mean_attn.sum(dim=-1, keepdim=True)
-        rollout = torch.matmul(rollout, mean_attn)
+        # Normalize the attention weights
+        attn /= attn.sum(dim=-1, keepdim=True)
 
-    print("Shape of rollout:", rollout.shape)
+        # Forward matrix multiplication
+        rollout = torch.matmul(rollout, attn)
 
-    attn = rollout[0, 1:, 0]
-    dimension = np.sqrt(rollout.shape[1] - 1)
-    attn = 1- attn.reshape(int(dimension), int(dimension))
-
-    # Plot the image and the attention weights side by side
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.imshow(image[0])
-    plt.axis('off')
-    plt.title("Image")
-    plt.subplot(1, 2, 2)
-    plt.title("Attention Weights")
-    plt.axis('off')
-    plt.imshow(attn.cpu().detach().numpy(), cmap='viridis')
-    plt.colorbar()
-    plt.show()
-
-def self_attention_rollout(model, text, image):
-    """
-    Perform self-attention rollout for a given model and inputs.
-
-    Args:
-        model: The model to perform self-attention rollout on. Must have a method called get_model_attention.
-        image: The image input for the model.
-        text: The text input for the model.
-    Returns:
-        Tensor: The attention weights from the model of dims (text_shape, image_shape).
-    """
-    # Assert the model has a method called get_model_attention
-    assert hasattr(model, 'get_model_attention'), "Model must have a method called get_model_attention"
-
-    # Move model to eval mode
-    model.eval()
-
-    # Forward pass through the model to get the attention weights
-    probs = model.predict(text, image)
-
-    # Get the prediction
-    pred = torch.round(probs).squeeze(0)
-    print("Prediction:", get_label_str_list(pred))
-    
-    # Get the dict of attention weights from the model
-    attn = model.get_model_attention()
-
-    # Unpack the dict
-    clip_text_attn = attn['clip_text']
-    clip_image_attn = attn['clip_image']
-    classifier_attn = attn['classifier']
-
-    print("Shapes of iterables:", clip_text_attn[0].shape, clip_image_attn[0].shape, classifier_attn[0].shape)
-
-    # Get the dimensions of the text and image attention matrices respectively
-    dim_text_mat = clip_text_attn[0].shape[2]
-    dim_image_mat = clip_image_attn[0].shape[2]
-    print("Dimensions of text and image attention matrices:", dim_text_mat, dim_image_mat, len(clip_text_attn))
-    print("Classifier attention shape:", classifier_attn[0].shape, len(classifier_attn))
+    # Print the shape of the final rollout attention weights
+    print("Shape of rollout:", rollout.shape, "\n")
 
     # Extract the attention weights for the image
-    attn_cumulative_tuple = [classifier_attn[0][:, :dim_image_mat, :dim_image_mat]]
-    print("Shape of clip image attention:", attn_cumulative_tuple[0].shape)
-    for i in range(1, len(classifier_attn)):
-        attn_cumulative_tuple.append(classifier_attn[i][:, :dim_image_mat, :dim_image_mat])
-        print("Shape of classifier attention:", classifier_attn[i].shape)
-    
-    print("\n")
-
-    # print("Shape of classifier attention:", attn_cumulative_tuple[0].shape, attn_cumulative_tuple[1].shape)
-    complete_attn_image = clip_image_attn + tuple(attn_cumulative_tuple)
-
-    # print("Shape of complete attention image:", complete_attn_image[-2].shape, complete_attn_image[-1].shape)
-
-    for ten in complete_attn_image:
-        print("Shape of attention tensor:", ten.shape)
-
-    print("\n")
-    
-    # Perform attention rollout
-    rollout = torch.eye(dim_image_mat).unsqueeze(0)
-    for ten in complete_attn_image:
-        mean_attn = ten
-        if len(mean_attn.shape) == 4: 
-            mean_attn = mean_attn.mean(dim=1)
-        
-        print("Shape of mean attention:", mean_attn.shape)
-        print("Shape of rollout:", rollout.shape)
-
-        id_mat = torch.eye(mean_attn.shape[1]).to(mean_attn.device)
-        mean_attn = mean_attn + id_mat.unsqueeze(0)
-
-        mean_attn /= mean_attn.sum(dim=-1, keepdim=True)
-        rollout = torch.matmul(rollout, mean_attn)
-        
-    print("Shape of rollout:", rollout.shape)
-
     attn = rollout[0, 1:, 0]
+
+    # Reshape the attention weights to a square matrix
     dimension = np.sqrt(rollout.shape[1] - 1)
-    attn = 1- attn.reshape(int(dimension), int(dimension))
+    attn = 1 - attn.reshape(int(dimension), int(dimension))
 
     # Plot the image and the attention weights side by side
     plt.figure(figsize=(10, 5))
@@ -243,18 +147,12 @@ def self_attention_rollout(model, text, image):
     plt.imshow(attn.cpu().detach().numpy(), cmap='viridis')
     plt.colorbar()
     plt.show()
-
-
-
-
-
 
 
 if __name__ == "__main__":
-    # path = "DL_vs_HateSpeech\models\model_checkpoints\ModelV2_single_class"
     path = "DL_vs_HateSpeech/models/model_checkpoints/ModelV2_single_class"
     
-
-    plot_attention_rollout(path, device="cpu")
+    # Call the function to plot attention rollout
+    plot_attention_rollout(path, self_attn=True, device="cpu")
     print("Attention rollout complete.")
 
