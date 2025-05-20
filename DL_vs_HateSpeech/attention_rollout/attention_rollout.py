3a2d17c767f78c5abfe9bb32bed3ebc167a4fc63
MattiaBarbiere <barbiere.mattia@gmail.com>
2025-05-20 13:33:33 +0200

    Images for model 32

--- DL_vs_HateSpeech/attention_rollout/attention_rollout.py
+++ DL_vs_HateSpeech/attention_rollout/attention_rollout.py
@@ -1,7 +1,7 @@
 from DL_vs_HateSpeech.attention_rollout.attention_plots import plot_attention_rollout
 
 # Selected model path
-path = "DL_vs_HateSpeech/models/model_checkpoints/ModelV2_single_class_clip_16"
+path = "DL_vs_HateSpeech/models/model_checkpoints/ModelV2_single_class_clip_32"
 
 # Selected indexes
 indexes = [3, 4, 8, 10, 16, 17, 25, 27, 35, 38, 813]
@@ -10,5 +10,5 @@ indexes = [3, 4, 8, 10, 16, 17, 25, 27, 35, 38, 813]
 for i in indexes:
     print(f"Processing image {i}...")
     plot_attention_rollout(path, self_attn=True, blur=False, alpha_image=0.5, index=i, 
-                        device="cpu", save_fig=True, show_fig=True)
+                        device="cpu", save_fig=True, show_fig=False)
     print("Attention rollout complete.")
